2012287521b282413754a672c1298eb431e1782a
Tzu-Hsien(Ben) Lee <lee04484@umn.edu>
2025-09-03 22:39:32 +0000

    Added Vjepa2 encoder; This version duplicate the same image twice

---
+++ models/vision_encoders/vjepa2.py
@@ -0,0 +1,74 @@
+import torch
+from Object_Centric_Local_Navigation.models.modules.utils import resize_and_normalize_tensor
+
+class Vjepa2(torch.nn.Module):
+    # TRANSFORM_SIZE = (476, 476)
+    # TRANSFORM_MEAN = [0.485, 0.456, 0.406]
+    # TRANSFORM_STD = [0.229, 0.224, 0.225]
+    PATCH_NUM = 16
+    EMBED_DIM = 1024
+
+    def __init__(self):
+        super().__init__()
+        self.processor = torch.hub.load('facebookresearch/vjepa2', 'vjepa2_preprocessor')
+        self.vjepa2, _ = torch.hub.load('facebookresearch/vjepa2', 'vjepa2_vit_large')
+        for param in self.vjepa2.parameters():
+            param.requires_grad = False
+        self.vjepa2.eval()
+
+    @torch.no_grad() 
+    def forward(self, batch_images):
+        """
+        Extract DINOv2 ebeddings
+
+        Parameters
+        ----------
+        batch_images : torch.Tensor
+            Batch of RGB images of shape (B, 3, H, W)
+        
+        Returns
+        -------
+        batch_embeddings : torch.Tensor
+            Patch embeddings of shape (B, C, P, P), where:
+                - C = `EMBED_DIM` (embedding dimension per patch token)
+                - P = `PATCH_NUM` (number of patches per spatial axis after resizing)
+            Each embedding corresponds to a specific spatial patch in the input image.
+        """
+        batch_size = batch_images.shape[0]
+        # batch_images = resize_and_normalize_tensor(batch_images, self.TRANSFORM_SIZE, self.TRANSFORM_MEAN, self.TRANSFORM_STD)
+
+        # Preprocess images 
+        batch_images_processed = []
+        for image in batch_images:
+            image = image.unsqueeze(0).expand(2, -1, -1, -1)
+            image = self.processor(image)[0]
+            batch_images_processed.append(image)
+        
+        batch_images_processed = torch.stack(batch_images_processed)
+        vjepa_output = self.vjepa2(batch_images_processed)
+        batch_embeddings = vjepa_output.reshape(batch_size, self.PATCH_NUM, self.PATCH_NUM, self.EMBED_DIM).permute(0, 3, 1, 2)
+
+        return batch_embeddings
+        
+if __name__ == '__main__':
+
+    import os
+    from PIL import Image
+    from torchvision import transforms
+
+    transform = transforms.Compose([
+            transforms.Resize([640, 480]),
+            transforms.ToTensor()])
+    
+    images_dir = ''
+    images = []
+    for i in range(4):
+        image = Image.open(os.path.join(images_dir, f'{i}.jpg'))
+        image = transform(image)
+        images.append(image)
+
+    images = torch.stack(images).to(device='cuda')
+
+    vision_encoder = Vjepa2().to(device='cuda')
+    embedding = vision_encoder(images)
+    print(embedding.shape)
---
+++ models/vjepa2_decoder1.py
@@ -0,0 +1,49 @@
+from Object_Centric_Local_Navigation.models.modules.base_model import BaseModel
+from Object_Centric_Local_Navigation.models.vision_encoders.vjepa2 import Vjepa2
+from Object_Centric_Local_Navigation.models.segmentation_models.owl_v2_sam2 import OwlV2Sam2
+from Object_Centric_Local_Navigation.models.action_decoders.decoder1 import Decoder1
+
+class Vjepa2Decoder1(BaseModel):
+
+    def __init__(self, use_embeddings=False):
+        
+        vision_encoder = Vjepa2()
+        segmentation_model = OwlV2Sam2()
+        action_decoder = Decoder1()
+        super().__init__(vision_encoder, segmentation_model, action_decoder, use_embeddings)
+
+if __name__ == '__main__':
+
+    import os, torch
+    from PIL import Image
+    from torchvision import transforms
+
+    transform = transforms.Compose([
+            transforms.Resize([640, 480]),
+            transforms.ToTensor()])
+
+    goal_images_dir = ''
+    current_image_dir = ''
+    prompt = ''
+
+    goal_images = []
+    current_images = []
+    for i in range(4):
+        current_image = Image.open(os.path.join(current_image_dir, f'{i}.jpg'))
+        current_image = transform(current_image)
+        current_images.append(current_image)
+
+        goal_image = Image.open(os.path.join(goal_images_dir, f'{i}.jpg'))
+        goal_image = transform(goal_image)
+        goal_images.append(goal_image)
+    current_images = torch.stack(current_images).to(device='cuda')
+    goal_images = torch.stack(goal_images).to(device='cuda')
+
+    model = Vjepa2Decoder1().to(device='cuda')
+    # weight_path = ''
+    # model.load_weight(weight_path)
+
+    with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
+        output, debug_info = model(current_images.unsqueeze(0), goal_images.unsqueeze(0), prompt)
+    output = torch.argmax(output, dim=2)
+    print(f'Output: {output}')
